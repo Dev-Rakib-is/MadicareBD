6091321d2a9ec7a0b9a2f40c366d5347b9f57bd1
Rakibul Islam <ri3390990@gmail.com>
2025-11-04 15:52:41 +0600

    cookieParser update

--- app.js
+++ app.js
@@ -16,9 +16,13 @@ connectDB();
 
 const app = express();
 
-app.use(cors({ 
-    origin:"http://localhost:5173",
-    credentials: true }));
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
+app.use(cookieParser());
 app.use(express.json());
 
 // Default route
