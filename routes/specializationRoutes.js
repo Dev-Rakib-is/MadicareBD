d0e7db1401b1b7d59162c4a31f88607f0756f055
Rakibul Islam <ri3390990@gmail.com>
2025-11-09 01:43:10 +0600

    specialization update

--- routes/specializationRoutes.js
+++ routes/specializationRoutes.js
@@ -1,12 +1,27 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
 const specializations = [
-  "Cardiologist",
-  "Dentist",
-  "Neurologist",
-  "Dermatologist",
-  "Gynecologist",
+  { _id: 1, name: "Cardiologist" },
+  { _id: 2, name: "Dentist" },
+  { _id: 3, name: "Neurologist" },
+  { _id: 4, name: "Dermatologist" },
+  { _id: 5, name: "Gynecologist" },
+  { _id: 6, name: "Pediatrician" },
+  { _id: 7, name: "Psychiatrist" },
+  { _id: 8, name: "Radiologist" },
+  { _id: 9, name: "Surgeon" },
+  { _id: 10, name: "General Physician" },
+  { _id: 11, name: "Orthopedic" },
+  { _id: 12, name: "Ophthalmologist (Eye Specialist)" },
+  { _id: 13, name: "ENT (Ear, Nose, Throat)" },
+  { _id: 14, name: "Urologist" },
+  { _id: 14, name: "Endocrinologist" },
+  { _id: 14, name: "Gastroenterologist" },
+  { _id: 14, name: "Pulmonologist (Lung Specialist)" },
+  { _id: 14, name: "Nephrologist (Kidney Specialist)" },
+  { _id: 14, name: "Oncologist (Cancer Specialist)" },
+  { _id: 14, name: "Rheumatologist" },
 ];
 
 router.get("/", (req, res) => {
@@ -14,3 +29,43 @@ router.get("/", (req, res) => {
 });
 
 module.exports = router;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
