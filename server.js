16c3d7f0e10e42c6a524587adb4aaf74d054da98
Rakibul Islam <ri3390990@gmail.com>
2025-11-09 17:03:33 +0600

    back old position

--- server.js
+++ server.js
@@ -1,21 +1,15 @@
-const express = require("express");
-const cors = require("cors");
-const cookieParser = require("cookie-parser");
 require("dotenv").config();
-
-const app = express();
-app.use(express.json());
-app.use(cookieParser());
+const app = require("./app");
+const cors = require("cors");
+const PORT = process.env.PORT || 5000;
 
 app.use(
   cors({
-    origin: ["https://medicare-bd.vercel.app", "http://localhost:5173"],
+    origin: ["https://madicarebd.vercel.app", "http://localhost:5173"],
     credentials: true,
   })
 );
 
-app.set("trust proxy", 1); 
+app.set("trust proxy", 1);
 
-app.listen(process.env.PORT || 5000, () =>
-  console.log(`Server running on port ${process.env.PORT || 5000}`)
-);
+app.listen(PORT, () => console.log(` Server running on port ${PORT}`));
