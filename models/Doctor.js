c834f7a535c38a7600030e18f33bd8689d4592f0
Rakibul Islam <ri3390990@gmail.com>
2025-11-21 12:57:13 +0600

    photo url add

--- models/Doctor.js
+++ models/Doctor.js
@@ -1,12 +1,14 @@
-const mongoose = require('mongoose');
-const doctorSchema = new mongoose.Schema({
-name: { type: String, required: true },
-specialization: { type: String, required: true },
-photo_url: String,
-rating: { type: Number, default: 0 },
-available_slots: [Date],
-user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' }
-},{ timestamps: true });
+const mongoose = require("mongoose");
+const doctorSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    specialization: { type: String, required: true },
+    photo_url: { type: String, required: true },
+    rating: { type: Number, default: 0 },
+    available_slots: [Date],
+    user: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+  },
+  { timestamps: true }
+);
 
-
-module.exports = mongoose.model('Doctor', doctorSchema);
+module.exports = mongoose.model("Doctor", doctorSchema);
